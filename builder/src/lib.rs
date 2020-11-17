dda85bf3ffa5e6a5e020a6248f29788d3b6960f1
Brian Shu <littlebubu.shu@gmail.com>
2020-11-17 17:19:05 -0500

    passed first 6 tests for builder

---
+++ builder/src/lib.rs
@@ -0,0 +1,117 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+fn ty_is_option(ty: &syn::Type) -> Option<&syn::Type> {
+    if let syn::Type::Path(ref p) = ty {
+        if p.path.segments.len() != 1 || p.path.segments[0].ident != "Option" {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(ref inner_ty) = p.path.segments[0].arguments {
+            if inner_ty.args.len() != 1 {
+                return None;
+            }
+
+            let innert_ty = inner_ty.args.first().unwrap();
+            if let syn::GenericArgument::Type(ref ty) = innert_ty {
+                return Some(ty);
+            }
+        }
+    }
+    None
+}
+
+#[proc_macro_derive(Builder)]
+pub fn derive_builder(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = syn::Ident::new(&bname, name.span());
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        unimplemented!();
+    };
+
+    let optionized = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if ty_is_option(ty).is_some() {
+            quote! { #name: #ty }
+        } else {
+            quote! { #name: std::option::Option<#ty> }
+        }
+    });
+
+    let methods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if let Some(inner_ty) = ty_is_option(ty) {
+            quote! {
+                pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        }
+    });
+
+    let build_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if ty_is_option(ty).is_some() {
+            quote! {
+                #name: self.#name.clone()
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+            }
+        }
+    });
+
+    let build_empty = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! {
+            #name: None
+        }
+    });
+
+    let expanded = quote! {
+        pub struct #bident {
+            #(#optionized,)*
+        }
+
+        impl #bident {
+            #(#methods)*
+
+            pub fn build(&self) -> std::result::Result<#name, Box<dyn std::error::Error>> {
+                Ok(#name {
+                    #(#build_fields,)*
+                })
+            }
+        }
+
+        impl #name {
+            fn builder() -> #bident {
+                #bident {
+                    #(#build_empty,)*
+                }
+            }
+        }
+    };
+
+    expanded.into()
+}
---
+++ main.rs
@@ -0,0 +1,36 @@
+// Write code here.
+//
+// To see what the code looks like after macro expansion:
+//     $ cargo expand
+//
+// To run the code:
+//     $ cargo run
+
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: Option<String>,
+}
+
+fn main() {
+    let command = Command::builder()
+        .executable("cargo".to_owned())
+        .args(vec!["build".to_owned(), "--release".to_owned()])
+        .env(vec![])
+        .build()
+        .unwrap();
+    assert!(command.current_dir.is_none());
+
+    let command = Command::builder()
+        .executable("cargo".to_owned())
+        .args(vec!["build".to_owned(), "--release".to_owned()])
+        .env(vec![])
+        .current_dir("..".to_owned())
+        .build()
+        .unwrap();
+    assert!(command.current_dir.is_some());
+}
