69e51f973de2fea1d4c6fce276c3e214b402e9c7
Brian Shu <littlebubu.shu@gmail.com>
2020-11-27 14:46:10 -0500

    refactored more

--- builder/src/expand.rs
+++ builder/src/expand.rs
@@ -12,82 +12,98 @@ pub fn derive(node: &DeriveInput) -> Result<TokenStream> {
     let input = Input::from_syn(node)?;
     input.validate()?;
     Ok(match input {
-        Input::Struct(input) => impl_struct(input),
+        Input::Struct(input) => input.impl_builder(),
         _ => unreachable!(),
     })
 }
 
-fn impl_struct(input: Struct) -> TokenStream {
-    let ident = &input.ident;
+impl<'a> Struct<'a> {
+    pub fn impl_builder(self) -> TokenStream {
+        let ident = &self.ident;
 
-    let builder_ident = {
-        let ident = &input.ident;
-        let builder_name = format!("{}Builder", &input.ident);
+        let builder_ident = self.builder_ident();
+
+        let optionized_fields = self.optionized_fields();
+
+        let methods = self.methods();
+
+        let builder_fields_empty = self.builder_fields_empty();
+
+        let expanded = quote! {
+            // create the builder struct with name builder_ident and optionized fields from the
+            // original struct
+            pub struct #builder_ident {
+                #(#optionized_fields,)*
+            }
+
+            // implement methods on to the builder struct that will set fields appropriately
+            impl #builder_ident {
+                // the regular methods that set individual fields
+                #(#methods)*
+
+                // extend methods
+            }
+
+            // implementations on the original struct (currently only the builder method)
+            impl #ident {
+                fn builder() -> #builder_ident {
+                    #builder_ident {
+                        #(#builder_fields_empty,)*
+                    }
+                }
+            }
+        };
+
+        expanded.into()
+    }
+
+    fn builder_ident(&self) -> Ident {
+        let ident = &self.ident;
+        let builder_name = format!("{}Builder", &self.ident);
         Ident::new(&builder_name, ident.span())
-    };
+    }
 
-    let optionized = input.fields.iter().map(|f| {
-        let ident = &f.ident;
-        let ty = &f.ty;
-        if ty_is_option(ty) {
-            quote! { #ident: #ty }
-        } else {
-            quote! { #ident: ::std::option::Option<#ty> }
-        }
-    });
+    fn optionized_fields(&'a self) -> impl Iterator<Item = TokenStream> + 'a {
+        self.fields.iter().map(|f| {
+            let ident = &f.ident;
+            let ty = &f.ty;
+            if ty_is_option(ty) {
+                quote! { #ident: #ty }
+            } else {
+                quote! { #ident: ::std::option::Option<#ty> }
+            }
+        })
+    }
 
-    let methods = input.fields.iter().map(|f| {
-        let ident = &f.ident;
-        let ty = &f.ty;
+    fn methods(&'a self) -> impl Iterator<Item = TokenStream> + 'a {
+        self.fields.iter().map(|f| {
+            let ident = &f.ident;
+            let ty = &f.ty;
 
-        if let Ok(inner_ty) = inner_ty(ty, "Option") {
-            quote! {
-                pub fn #ident(&mut self, #ident: #inner_ty) -> &mut Self {
-                    self.#ident = Some(#ident);
-                    self
+            if let Ok(inner_ty) = inner_ty(ty, "Option") {
+                quote! {
+                    pub fn #ident(&mut self, #ident: #inner_ty) -> &mut Self {
+                        self.#ident = Some(#ident);
+                        self
+                    }
+                }
+            } else {
+                quote! {
+                    pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                        self.#ident = Some(#ident);
+                        self
+                    }
                 }
             }
-        } else {
-            quote! {
-                pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                    self.#ident = Some(#ident);
-                    self
-                }
-            }
-        }
-    });
+        })
+    }
 
-    let builder_fields_empty = input.fields.iter().map(|f| {
-        let ident = &f.ident;
-        quote! { #ident: None }
-    });
-
-    let expanded = quote! {
-        // create the builder struct with name builder_ident and optionized fields from the
-        // original struct
-        pub struct #builder_ident {
-            #(#optionized,)*
-        }
-
-        // implement methods on to the builder struct that will set fields appropriately
-        impl #builder_ident {
-            // the regular methods that set individual fields
-            #(#methods)*
-
-            // extend methods
-        }
-
-        // implementations on the original struct (currently only the builder method)
-        impl #ident {
-            fn builder() -> #builder_ident {
-                #builder_ident {
-                    #(#builder_fields_empty,)*
-                }
-            }
-        }
-    };
-
-    expanded.into()
+    fn builder_fields_empty(&'a self) -> impl Iterator<Item = TokenStream> + 'a {
+        self.fields.iter().map(|f| {
+            let ident = &f.ident;
+            quote! { #ident: None }
+        })
+    }
 }
 
 /// Checks if the the type has the ident of the wrapper. If it does, give the inner type of the
@@ -139,21 +155,4 @@ fn ty_is_option(ty: &Type) -> bool {
         PathArguments::AngleBracketed(bracketed) => bracketed.args.len() == 1,
         _ => false,
     }
-    // if let syn::Type::Path(ref p) = ty {
-    //     if p.path.segments.len() != 1 || p.path.segments[0].ident != "Option" {
-    //         return None;
-    //     }
-
-    //     if let syn::PathArguments::AngleBracketed(ref inner_ty) = p.path.segments[0].arguments {
-    //         if inner_ty.args.len() != 1 {
-    //             return None;
-    //         }
-
-    //         let innert_ty = inner_ty.args.first().unwrap();
-    //         if let syn::GenericArgument::Type(ref ty) = innert_ty {
-    //             return Some(ty);
-    //         }
-    //     }
-    // }
-    // None
 }
